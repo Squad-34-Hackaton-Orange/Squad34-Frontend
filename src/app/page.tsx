546a5b4198e9fc289c7c54f3aa174b98b16e3de4
Livia Helen <liviahelen025@gmail.com>
2024-01-23 09:10:46 -0300

    Set font weight as 400 and improve variables.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import styles from "./page.module.scss";
-
 export default function Home() {
   return (
-    <main className={styles.title}>
+    <main>
       <h4>Home Page</h4>
     </main>
   );
