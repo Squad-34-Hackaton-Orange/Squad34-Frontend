5c398694745f0f8b178beb33a44dd8a05290a1b9
Livia Helen <liviahelen025@gmail.com>
2024-01-26 18:35:32 -0300

    Refactor signup view code.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,7 +5,6 @@ import { useTheme } from '@mui/material/styles';
 import {
   Box,
   Button,
-  Container,
   FormControl,
   Grid,
   IconButton,
@@ -15,138 +14,150 @@ import {
   TextField,
   Typography
 } from '@mui/material';
+
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { Google } from '@/components/Icons/Google';
-import Image from 'next/image';
 
 export default function SignUp() {
   const [showPassword, setShowPassword] = React.useState(false);
   const theme = useTheme();
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
   return (
-    <Grid container sx={{ maxWidth: "xl" }}>
-      <Grid item
+    <Grid container columns={16}>
+      <Grid
+        item
+        xs={6}
         sx={{
-          width: '60%',
-          [theme.breakpoints.down("lg")]: {
-            display: "none",
-          }
+          display: { xs: "none", lg: "block" }
         }}
       >
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="flex-start"
-          minHeight="100vh"
-        >
-          <Image
-            src="/signup.svg"
-            alt="Orange Portfólio"
-            width={549} height={832}
-          />
-        </Box>
+        <img
+          src="./signup.svg"
+          alt="Imagem de login com três pessoas e um computador"
+          style={{
+            minHeight: "100vh",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
       </Grid>
 
       <Grid
         item
-        sx={{
-          width: '40%',
-          paddingRight: "1rem",
-          [theme.breakpoints.down("lg")]: {
-            width: "100%",
-          }
-        }}
+        xs={16}
+        lg={10}
       >
-        <Container
+        <Box
           sx={{
-            width: '100%',
-            [theme.breakpoints.up("lg")]: {
-              padding: "0px",
-            }
+            minHeight: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: { xs: "0 1rem" }
           }}
         >
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            gap={4}
-            minHeight="100vh"
+          <Typography
+            sx={{
+              marginBottom: 2,
+              textAlign: "center",
+              color: theme.colors.primary90,
+              typography: {
+                xs: "h5",
+                lg: "h3",
+              },
+            }}
           >
-            <Typography
-              variant="h5"
-              color={theme.colors.primary90}
-              sx={{
-                [theme.breakpoints.up("lg")]: {
-                  typography: "h3",
-                  color: theme.colors.primary90
-                },
-              }}
-            >
-              Cadastre-se
-            </Typography>
+            Cadastre-se
+          </Typography>
 
-            <Box>
-
-              <Box display='flex' gap={1} margin="normal" sx={{
-                [theme.breakpoints.down("sm")]: {
-                  flexDirection: "column",
-                  gap: 2
-                }
-              }}>
-                <FormControl fullWidth>
-                  <TextField id="name" aria-label="name" label="Nome" />
-                </FormControl>
-
-                <FormControl fullWidth>
-                  <TextField id="lastName" aria-label="lastName" label="Sobrenome" />
-                </FormControl>
-              </Box>
-
-              <FormControl fullWidth 
-              sx={{
-                marginTop: "1rem"
-              }}>
-                <TextField id="email" aria-label="email" label="Email address" />
-              </FormControl>
-
-              <FormControl variant="outlined" fullWidth               
-              sx={{
-                marginTop: "1rem"
-              }}>
-                <InputLabel htmlFor="password">Password</InputLabel>
-                <OutlinedInput
-                  id="password"
-                  type={showPassword ? 'text' : 'password'}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="Password"
-                        onClick={handleClickShowPassword}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Password"
+          <Box>
+            <Box display="flex" gap={1}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel
+                  htmlFor="name"
+                  style={{ visibility: "hidden" }}
+                >
+                  Nome
+                </InputLabel>
+                <TextField
+                  required
+                  id="name"
+                  aria-label="name"
+                  label="Nome"
                 />
               </FormControl>
 
-              <Button variant="contained" fullWidth size="large"
-                sx={{
-                  color: theme.colors.neutral60,
-                  backgroundColor: theme.colors.secondary100,
-                  marginTop: "1rem",
-                }}
-              >
-                Cadastrar
-              </Button>
+              <FormControl fullWidth margin="normal">
+                <InputLabel
+                  htmlFor="lastName"
+                  style={{ visibility: "hidden" }}
+                >
+                  Sobrenome
+                </InputLabel>
+                <TextField
+                  required
+                  id="lastName"
+                  aria-label="name"
+                  label="Sobrenome"
+                />
+              </FormControl>
             </Box>
+
+            <FormControl fullWidth margin="normal">
+              <InputLabel
+                htmlFor="email"
+                style={{ visibility: "hidden" }}
+              >
+                Email
+              </InputLabel>
+              <TextField
+                required
+                id="email"
+                aria-label="email"
+                label="Email Address"
+              />
+            </FormControl>
+
+            <FormControl variant="outlined" fullWidth margin="normal">
+              <InputLabel
+                required
+                htmlFor="password"
+              >
+                Password
+              </InputLabel>
+              <OutlinedInput
+                label="Password"
+                id="password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="Password"
+                      onClick={() => setShowPassword((show) => !show)}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+
+            <Button
+              variant="contained"
+              fullWidth
+              size="large"
+              sx={{
+                color: theme.colors.neutral60,
+                backgroundColor: theme.colors.secondary100,
+                margin: ".8rem 0",
+              }}
+            >
+              Cadastrar
+            </Button>
           </Box>
-        </Container>
+        </Box>
       </Grid>
     </Grid>
   )
