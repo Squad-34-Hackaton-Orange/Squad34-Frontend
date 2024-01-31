22415e55172e02bf7508b6350d78b88fbf7eb5f2
Juliobz <bzjuliocarmo@hotmail.com>
2024-01-31 17:56:33 -0300

    feat: VTextField exibe mensagens de erro e lida com teclas pressioandas

---
+++ src/forms/VTextField.tsx
@@ -0,0 +1,36 @@
+import { TextField, TextFieldProps } from "@mui/material";
+import { useField } from "@unform/core";
+import { useEffect, useState } from "react";
+
+type VtextFieldProps = TextFieldProps & {
+  name: string;
+};
+
+export const VTextField = ({ name, ...rest }: VtextFieldProps) => {
+  const { fieldName, registerField, defaultValue, error, clearError } = useField(name);
+  const [value, setValue] = useState(defaultValue || '');
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      getValue: () => value,
+      setValue: (_, newValue) => setValue(newValue),
+    });
+  }, [registerField, fieldName, value]);
+
+  return (
+    
+    <TextField
+      
+      
+      error={!!error}
+      helperText={error}
+      defaultValue={defaultValue}
+
+      label={rest.label}
+      value={value}
+      onChange={e => setValue(e.target.value)}
+      onKeyDown={() => error ? clearError() : undefined}
+    />
+  );
+};
