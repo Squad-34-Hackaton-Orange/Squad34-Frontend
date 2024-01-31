34f777323c06a3d5cf09ab2198432c598e8e89c1
Juliobz <bzjuliocarmo@hotmail.com>
2024-01-31 18:00:44 -0300

    feat: VOutlinedInput exibe mensagens de erro e lida com teclas pressioandas

---
+++ src/forms/VOutlinedInput.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import {
+  OutlinedInput,
+  InputLabel,
+  FormControl,
+  FormHelperText,
+  OutlinedInputProps
+} from "@mui/material";
+import { useField } from "@unform/core";
+
+type VOutlinedInputProps = OutlinedInputProps & {
+  name: string;
+  label?: string;
+};
+
+export const VOutlinedInput = ({ name, label, ...rest }: VOutlinedInputProps) => {
+  const { fieldName, registerField, defaultValue, error, clearError } = useField(name);
+  const [value, setValue] = useState(defaultValue ?? "");
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      getValue: () => value,
+      setValue: (_, newValue) => setValue(newValue),
+    });
+  }, [registerField, fieldName, value]);
+
+  const handleKeyDown = () => {
+    if (error) {
+      clearError();
+    }
+  };
+
+  return (
+    <FormControl fullWidth variant="outlined" error={!!error}>
+      {label && <InputLabel required htmlFor={name}>{label}</InputLabel>}
+      <OutlinedInput
+        id={name}
+        label={label}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={handleKeyDown}
+        {...rest}
+      />
+      {error && <FormHelperText>{error}</FormHelperText>}
+    </FormControl>
+  );
+};
