9ef3584fafd61a13c65b0aab12c4e9036791003f
Livia <liviahelen025@gmail.com>
2024-02-01 16:51:56 -0300

    Interceptor response api.

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -3,3 +3,26 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+
+export class Unauthorized extends Error {
+  constructor() {
+    super("Unauthorized");
+  }
+}
+
+export class NotFoundError extends Error { };
+
+export class BadRequest extends Error { };
+
+api.interceptors.response.use(undefined, async (error) => {
+  switch (error.response?.status) {
+    case 401:
+      throw new Unauthorized();
+    case 404:
+      throw new NotFoundError();
+    case 400:
+      throw new BadRequest();
+    default:
+      throw error;
+  }
+});
