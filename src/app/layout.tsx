43d3a005f02a51caf60adb9a4c6954d01634e4a8
Livia <liviahelen025@gmail.com>
2024-02-03 23:43:00 -0300

    Change title.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,14 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/styles/theme";
 import { LoginProvider } from "@/context/UserContext";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Portifólio',
+  description:
+    'Programa de Formação FCAMARA v5',
+};
+
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
